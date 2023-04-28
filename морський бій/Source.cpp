d4f0951d586d2a2fc5324a50e54ef64f61be9138
tolik vasylov <tolik.vasilov174@gmail.com>
2023-04-28 15:33:25 +0300

    Добавьте файлы проекта.

---
+++ морський бій/Source.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include<Windows.h>
+
+using namespace std;
+
+const int BOARD_SIZE = 10;
+
+class Ship {
+public:
+    Ship(int size) : size(size), hits(0) {}
+    bool isSunk() const {
+        return hits == size;
+    }
+    void hit() {
+        ++hits;
+    }
+private:
+    int size;
+    int hits;
+};
+
+class Board {
+public:
+    Board() {
+        for (int i = 0; i < BOARD_SIZE; ++i) {
+            board.push_back(vector<bool>(BOARD_SIZE, false));
+        }
+    }
+    bool hasShip(int x, int y) const {
+        return board[x][y];
+    }
+    bool fire(int x, int y) {
+        if (hasShip(x, y)) {
+            ships[x][y].hit();
+            board[x][y] = false;
+            return true;
+        }
+        return false;
+    }
+    bool addShip(int x, int y, int size, bool horizontal) {
+        if (x < 0 || x >= BOARD_SIZE || y < 0 || y >= BOARD_SIZE) {
+            return false;
+        }
+        if (horizontal && x + size > BOARD_SIZE) {
+            return false;
+        }
+        if (!horizontal && y + size > BOARD_SIZE) {
+            return false;
+        }
+        for (int i = 0; i < size; ++i) {
+            if (hasShip(x + (horizontal ? i : 0), y + (horizontal ? 0 : i))) {
+                return false;
+            }
+        }
+        for (int i = 0; i < size; ++i) {
+            board[x + (horizontal ? i : 0)][y + (horizontal ? 0 : i)] = true;
+            ships[x + (horizontal ? i : 0)][y + (horizontal ? 0 : i)] = Ship(size);
+        }
+        return true;
+    }
+    bool isGameOver() const {
+        for (int x = 0; x < BOARD_SIZE; ++x) {
+            for (int y = 0; y < BOARD_SIZE; ++y) {
+                if (hasShip(x, y) && !ships[x][y].isSunk()) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+private:
+    vector<vector<bool>> board;
+    vector<vector<Ship>> ships;
+};
+
+class Game {
+public:
+    Game() {}
+
+    Board playerBoard();
+    Board computerBoard();
+
+    void play()
+    {
+        SetConsoleCP(1251);
+        SetConsoleOutputCP(1251);
+
+        cout << "Вітаю в грі морський бій " << endl;
+        cout << "Ви граєте з компютером успіхів";
+    }
+}
